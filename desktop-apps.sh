fb656b914764b3b81cd9b8b749212dbaa11ac33c
Simon Heath <icefoxen@gmail.com>
2014-09-08 21:21:30 +0000

    Added pavucontrol

--- desktop-apps.sh
+++ desktop-apps.sh
@@ -4,7 +4,7 @@
 
 PACKAGES="dosbox zsnes firefox virtualbox libreoffice audacious bittornado ddd
 qgis inkscape leafpad monodevelop pcsxr recordmydesktop visualboyadvance vlc 
-mplayer virtualbox-qt virtualbox-dkms scrot"
+mplayer virtualbox-qt virtualbox-dkms scrot pavucontrol"
 
 aptitude install $PACKAGES
 
